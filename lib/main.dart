7e27bdcab9fb285fd75900f7c93461b015d9b45a
sup3rcow <bullandrooster@gmail.com>
2022-04-19 11:34:41 +0200

    theme manager

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'app/app.dart';
+import 'package:complete_advanced_flutter/app/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
--- lib/presentation/color_manager.dart
+++ lib/presentation/color_manager.dart
@@ -6,6 +6,13 @@ class ColorManager {
   static const Color grey = Color(0xFF737477);
   static const Color lightGrey = Color(0xFF9E9E9E);
   static const Color primaryOpacity70 = Color(0xB3ED9728);
+
+  // new colors
+  static const Color darkPrimary = Color(0xFFD17D11);
+  static const Color grey1 = Color(0xFF707070);
+  static const Color grey2 = Color(0xFF797979);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color error = Color(0xFFE61F34);
 }
 
 // HexColor.fromHex("#0xFFED9728") -  explicitno koristenje odrdjene extension static metode, nemoj tako nikada koristiti extension method?!
