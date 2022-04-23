6b333cd423be35b61ba7568d9f4809d50358c65b
sup3rcow <bullandrooster@gmail.com>
2022-04-23 13:07:15 +0200

    cardTheme, appBarTheme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,4 +1,7 @@
 import 'package:complete_advanced_flutter/presentation/color_manager.dart';
+import 'package:complete_advanced_flutter/presentation/font_manager.dart';
+import 'package:complete_advanced_flutter/presentation/styles_manager.dart';
+import 'package:complete_advanced_flutter/presentation/values_manager.dart';
 import 'package:flutter/material.dart';
 
 ThemeData getApplicationTheme() {
@@ -8,11 +11,28 @@ ThemeData getApplicationTheme() {
     primaryColorLight: ColorManager.primaryOpacity70,
     primaryColorDark: ColorManager.darkPrimary,
     disabledColor: ColorManager.grey1,
+    // ripple color
+    splashColor: ColorManager.primaryOpacity70,
     accentColor: ColorManager.grey,
+
     // card view theme
+    cardTheme: const CardTheme(
+      color: ColorManager.white,
+      shadowColor: ColorManager.grey,
+      elevation: AppSize.s4,
+    ),
 
     // App bar theme
-
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      color: ColorManager.primary,
+      elevation: AppSize.s4,
+      shadowColor: ColorManager.primaryOpacity70,
+      titleTextStyle: getRegularStyle(
+        color: ColorManager.white,
+        fontSize: FontSize.s16,
+      ),
+    ),
     // Button theme
 
     // Text theme
