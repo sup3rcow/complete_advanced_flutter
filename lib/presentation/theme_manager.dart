7a7e78df8bb62788254cc7cb1aca1659c283de92
sup3rcow <bullandrooster@gmail.com>
2022-04-23 13:22:31 +0200

    button text input theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -33,10 +33,85 @@ ThemeData getApplicationTheme() {
         fontSize: FontSize.s16,
       ),
     ),
+
     // Button theme
+    buttonTheme: const ButtonThemeData(
+      shape: StadiumBorder(),
+      disabledColor: ColorManager.grey1,
+      buttonColor: ColorManager.primary,
+      splashColor: ColorManager.primaryOpacity70,
+    ),
+
+    // elevated button theme
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        textStyle: getRegularStyle(color: ColorManager.white),
+        primary: ColorManager.primary,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(AppSize.s12),
+        ),
+      ),
+    ),
 
     // Text theme
+    textTheme: TextTheme(
+      headline1: getSemiBoldStyle(
+        color: ColorManager.darkGrey,
+        fontSize: FontSize.s16,
+      ),
+      subtitle1: getMediumStyle(
+        color: ColorManager.lightGrey,
+        fontSize: FontSize.s14,
+      ),
+      caption: getRegularStyle(
+        color: ColorManager.grey1,
+      ),
+      bodyText1: getRegularStyle(
+        color: ColorManager.grey,
+      ),
+    ),
 
     // input decoration theme (text form field)
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: const EdgeInsets.all(AppPadding.p8),
+      // hint style
+      hintStyle: getRegularStyle(color: ColorManager.grey1),
+
+      // label style
+      labelStyle: getMediumStyle(color: ColorManager.darkGrey),
+      // error style
+      errorStyle: getRegularStyle(color: ColorManager.error),
+
+      // enabled border
+      enabledBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: ColorManager.grey, width: AppSize.s1_5),
+          borderRadius: BorderRadius.all(Radius.circular(AppSize.s8))),
+
+      // focused border
+      focusedBorder: const OutlineInputBorder(
+        borderSide: BorderSide(
+          color: ColorManager.primary,
+          width: AppSize.s1_5,
+        ),
+        borderRadius: BorderRadius.all(Radius.circular(AppSize.s8)),
+      ),
+
+      // error border
+      errorBorder: const OutlineInputBorder(
+        borderSide: BorderSide(
+          color: ColorManager.error,
+          width: AppSize.s1_5,
+        ),
+        borderRadius: BorderRadius.all(Radius.circular(AppSize.s8)),
+      ),
+      // focused error border
+      focusedErrorBorder: const OutlineInputBorder(
+        borderSide: BorderSide(
+          color: ColorManager.primary,
+          width: AppSize.s1_5,
+        ),
+        borderRadius: BorderRadius.all(Radius.circular(AppSize.s8)),
+      ),
+    ),
   );
 }
